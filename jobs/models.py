d4e52e1eec167b9fde3b94ceb58a58686623807e
Anket Waswani <anket.m@live.com>
2019-04-30 09:23:13 +0400

    back to just having a page that shows the python projects, now going to add a blog

--- jobs/models.py
+++ jobs/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
+
+    def summary(self):
+        return self.title[:100]
+
+    def __str__(self):
+        return self.title  
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
